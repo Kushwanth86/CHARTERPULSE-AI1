ce72e53b3f0681877b07969b7e6365a68ee2eab1
Kushwanth <24031100434@reva.edu.in>
2026-09-10 07:48:43 +0530

    feat(web): load final executive overrides

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./decision-intelligence.css";
 import "./professional-ui.css";
 import "./enterprise-dashboard.css";
+import "./executive-overrides.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
